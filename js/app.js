fe3639abd79c9a100ce9f9c0da5c931a61d82e82
alam-gir <info.alamgirhussain@gmail.com>
2022-11-02 20:26:30 +0600

    add notes complete

--- js/app.js
+++ js/app.js
@@ -1,29 +1,156 @@
-// seclect global element
+// seclect global element=======================================
 
 const addBtn = document.querySelector('.add-notes');
 const formContainer = document.querySelector('.form-container');
 const formSubmitBtn = document.getElementById('submit-btn');
 
+// for notes card btns animation 
+
+const notesArea = document.querySelector('.notes-area');
+
+
+
+// user inputed value 
+const userInputForm = document.querySelector('#user-input-form');
+const inputedTittle = userInputForm[name="input-tittle"];
+const inputedText= userInputForm[name="input-text"];
+
+// notes container for append all notes 
+const notesContainer = document.querySelector('.notes-container');
+
+
+// all notes from localStorage 
+const allNotes = JSON.parse(localStorage.getItem("allNotes")) || [];
+
+
+// event =======================================
+
+window.addEventListener('DOMContentLoaded',()=>{
+    showNotes();
+    console.log("abc");
+
+    
+})
+
 
 // add notes btn evetn 
-addBtn.addEventListener('click', userInputFormOpen)
+addBtn.addEventListener('click', (e)=>{
+    userInputFormShow(e);
+})
+
+
 
 // submit/backward btn event
-formSubmitBtn.addEventListener('click', userInputFormSubmit);
+formSubmitBtn.addEventListener('click', userInputFormHide);
+formSubmitBtn.addEventListener('click', addNotes);
+formSubmitBtn.addEventListener('click', showNotes);
+
+
+
+
+
+
+
+
+
+
+// function =======================================
+
 
 // open user input form function
-function userInputFormOpen(){
+function userInputFormShow(e){
     formContainer.style.top = 0;
     formContainer.style.left = 0;
     formContainer.style.borderRadius = 0;
-    formContainer.style.overFlow = inherit;
+    inputedTittle.value = "";
+    inputedText.value = "";
 
 }
 // close user input form function
-function userInputFormSubmit(){
+function userInputFormHide(){
     formContainer.style.top = "-100%";
     formContainer.style.left = "-100%";
     formContainer.style.borderRadius = "100rem";
-    formContainer.style.overFlow = hidden;
+
+}
+
+
+
+// notes entry object make for easy 
+const notesCreate = function (notesTittle, notesText){
+    this.notesTittle = notesTittle;
+    this.notesText = notesText;
+    
+    if(notesTittle == ""){
+        notesTittle = "Untittled...";
+    }
+    return( entry = {tittle:notesTittle, notesText: notesText})
+}
+
+// submit notes 
+function addNotes(){
+    // get previous data from localStorage if no data take an empty arry
+    const previousNotes = allNotes || [];
+
+    // make notes by entry object 
+
+    let notes = new notesCreate(inputedTittle.value, inputedText.value);
+    previousNotes.push(notes);
+
+    // set data in localStorage 
+    localStorage.setItem("allNotes", JSON.stringify(previousNotes));
+}
+
+
+// show notes card on home
+function showNotes(){
+                const notesElement = allNotes.map(item=>{
+                    return `<div class="notes-card">
+                    <div class="notes-btns">
+                        <span class="material-icons-round">edit_note</span>
+                        <span class="material-icons-outlined">delete</span>
+                    </div>
+                    <article class="notes-area">
+                        <h2 class="notes-tittle">${item.tittle}</h2>
+                        <p class="notes-text">${item.notesText}</span></p>
+                    </article>
+                </div>`
+                }).join("")
+            
+                notesContainer.innerHTML = notesElement;
+
+                // car btns system apear here because of the card will apear in dom after this showNotes function run 
+                cardBtnsSystem();
+}
+
+
+// card btn functions 
+function cardBtnsSystem(){
+    // card btns dunctions
+    const notesCard = document.querySelectorAll('.notes-card');
+
+    // notes card mouseOver Out action 
+    notesCard.forEach(card => {
+
+
+        // each card mouseover and mouseout event
+        card.addEventListener('mouseover', e=>{
+            const notesCardBtns = e.currentTarget.childNodes[1];
+            const notesAreaMove = e.currentTarget.childNodes[3];
+
+            notesCardBtns.style.top = ".5rem";
+            notesAreaMove.style.top = "1rem";
+            console.log('alhamdulillah');
+
+        })
+        card.addEventListener('mouseout', e=>{
+            const notesCardBtns = e.currentTarget.childNodes[1];
+            const notesAreaMove = e.currentTarget.childNodes[3];
+
+            notesCardBtns.style.top = "-2rem";
+            notesAreaMove.style.top = "0";
+
+        })
+    });
 
 }
